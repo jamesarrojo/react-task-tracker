ece876f9ba6e46045a7fc0c292fae8022d265389
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-06-16 10:54:26 +0800

    added Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
-const tasks = [
-    
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} />
+            ))}
+        </>
     )
 }
 
